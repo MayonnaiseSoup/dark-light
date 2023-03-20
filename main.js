4cf55bded384e6fe7b7aa1f97795619557023389
MayonnaiseSoup <93639973+MayonnaiseSoup@users.noreply.github.com>
2023-03-20 14:27:47 +0800

    Updated so it supports all machines

--- main.js
+++ main.js
@@ -9,12 +9,12 @@ function switchMode() {
     if (isDarkMode) {
         isDarkMode = false
         document.getElementById("Title").innerText = "Light Mode"
-        document.getElementById("image").src = "/Users/27corm/Desktop/Everything/Coding/Dark:Light-mode/images/sunrise.png"
+        document.getElementById("image").src = "./images/sunrise.png"
         document.getElementById("body").className = "light"
     } else {
         isDarkMode = true
         document.getElementById("Title").innerText = "Dark Mode"
-        document.getElementById("image").src = "/Users/27corm/Desktop/Everything/Coding/Dark:Light-mode/images/moon.png"
+        document.getElementById("image").src = "./images/moon.png"
         document.getElementById("body").className = "dark"
     }
 }
